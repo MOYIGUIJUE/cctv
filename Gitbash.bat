ecb8035314e98314daaa3f4558e4df73970757fc
CCTV <3058084277@qq.com>
2022-07-21 01:32:08 +0800

    TT

--- Gitbash.bat
+++ Gitbash.bat
@@ -3,6 +3,7 @@ rem mode 70,15
 REM setlocal enabledelayedexpansion
 REM git init
 REM git remote add origin https://github.com/MOYIGUIJUE/cctv.git
+REM git remote add origin git@github.com:MOYIGUIJUE/cctv.git
 REM git remote -v
 REM git pull --rebase origin master
 if "%~1"=="" (
@@ -10,8 +11,8 @@ if "%~1"=="" (
 	exit /b
 )
 REM git diff
-git add *
-git commit -m "%~1"
+git add .
+git commit -m %1
 git push origin master
 REM git push -u origin master
 REM git pull --rebase origin master
@@ -37,3 +38,54 @@ REM git push origin --delete Chapater6
 
 REM 删除本地分支 Chapater6
 REM git branch -d  Chapater6
+exit /b
+
+
+521xueweihan/GitHub520
+
+# GitHub520 Host Start
+140.82.112.4                  alive.github.com
+140.82.112.4                  live.github.com
+54.82.108.91                  github.githubassets.com
+140.82.112.4                  central.github.com
+18.208.186.94                 desktop.githubusercontent.com
+140.82.112.4                  assets-cdn.github.com
+54.209.92.141                 camo.githubusercontent.com
+151.101.1.6                   github.map.fastly.net
+151.101.1.6                   github.global.ssl.fastly.net
+140.82.112.4                  gist.github.com
+185.199.108.153               github.io
+140.82.112.4                  github.com
+192.0.66.2                    github.blog
+140.82.112.4                  api.github.com
+54.224.236.218                raw.githubusercontent.com
+18.209.98.126                 user-images.githubusercontent.com
+34.239.174.105                favicons.githubusercontent.com
+44.199.232.135                avatars5.githubusercontent.com
+3.84.129.41                   avatars4.githubusercontent.com
+54.91.59.202                  avatars3.githubusercontent.com
+54.210.116.246                avatars2.githubusercontent.com
+3.89.61.163                   avatars1.githubusercontent.com
+3.236.118.112                 avatars0.githubusercontent.com
+44.200.134.42                 avatars.githubusercontent.com
+140.82.112.4                  codeload.github.com
+72.21.206.80                  github-cloud.s3.amazonaws.com
+72.21.206.80                  github-com.s3.amazonaws.com
+72.21.206.80                  github-production-release-asset-2e65be.s3.amazonaws.com
+72.21.206.80                  github-production-user-asset-6210df.s3.amazonaws.com
+72.21.206.80                  github-production-repository-file-5c1aeb.s3.amazonaws.com
+185.199.108.153               githubstatus.com
+64.71.144.211                 github.community
+23.100.27.125                 github.dev
+140.82.112.4                  collector.github.com
+35.174.62.133                 pipelines.actions.githubusercontent.com
+34.207.228.32                 media.githubusercontent.com
+44.202.131.196                cloud.githubusercontent.com
+18.234.174.253                objects.githubusercontent.com
+13.107.213.51                 vscode.dev
+
+
+# Update time: 2022-07-20T12:19:33+08:00
+# Update url: https://raw.hellogithub.com/hosts
+# Star me: https://github.com/521xueweihan/GitHub520
+# GitHub520 Host End
